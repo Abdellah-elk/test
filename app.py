8151a4b79ff63a0ba1177bbb36e3db4474cf7103
Abdellah-elk <146072073+Abdellah-elk@users.noreply.github.com>
2024-06-08 00:25:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for 
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/aakar_estate.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Property(db.Model):
@@ -11,27 +10,13 @@ class Property(db.Model):
     title = db.Column(db.String(80), nullable=False)
     description = db.Column(db.Text, nullable=False)
     price = db.Column(db.Float, nullable=False)
-    image_url = db.Column(db.String(200), nullable=False)
 
 @app.route('/')
-def home():
+def index():
     properties = Property.query.all()
     return render_template('index.html', properties=properties)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/property/<int:property_id>')
-def property_detail(property_id):
-    property = Property.query.get_or_404(property_id)
-    return render_template('property_detail.html', property=property)
-
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
+
 
